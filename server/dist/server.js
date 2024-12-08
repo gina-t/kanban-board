b5955821c9bfc01b4780f1a7727812deb9267ed0
Tao <ginadrcoder@gmail.com>
2024-12-08 17:04:37 +1100

    modify readme.md, run build

--- server/dist/server.js
+++ server/dist/server.js
@@ -4,14 +4,16 @@ import express from 'express';
 import cors from 'cors';
 import routes from './routes/index.js';
 import { sequelize } from './models/index.js';
+// import path from 'path';
 const app = express();
 const PORT = process.env.PORT || 3001;
 const forceDatabaseRefresh = false;
 //middleware
 app.use(cors({ origin: 'http://localhost:3000' }));
 app.use(express.json());
-//serve static files in the entire client's dist folder
+//serve static files from client's dist folder
 app.use(express.static('../client/dist'));
+// app.use(express.static(path.join(__dirname, '../client/dist')));
 //routes
 app.use(routes);
 // Error handling middleware
